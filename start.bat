dd4b652feb5c529f76427354ebfae4166196d6a6
ZXR0602 <changye_0624@qq.com>
2026-06-06 22:15:32 +0800

    Fix paths and update SCUM server installation script

--- start.bat
+++ start.bat
@@ -1,20 +1,20 @@
 @echo off
-set ServerPath-(: Path TolYour iSCUMBinaries Win64
-set SteamCMDPath-(:PathlTolStcomCNO
-set InstallPath-(:PathiTolSCUMlServerlInstoll
+set ServerPath=C:\Path\To\Your\SCUM\Binaries\Win64
+set SteamCMDPath=C:\Path\To\SteamCMD
+set InstallPath=C:\Path\To\SCUM\Server\Install
 
-：：Check and update SCUM server
-"XSteanCMDPath%)steancmd, exe" tforce_instoll_dir "%InstollPath%" +login anonymous tapp_update 3792589 +quit
+:: Check and update SCUM server
+"%SteamCMDPath%\steamcmd.exe" +force_install_dir "%InstallPath%" +login anonymous +app_update 3792580 +quit
 
-cd /d "XServerPath%"
-start SCUMServer, exe -log
+cd /d "%ServerPath%"
+start SCUMServer.exe -log
 
-:: Additionml startup arguments;
+:: Additional startup arguments:
 ::
-:：-port-7000           ：Start server on specified port (defoult is 7779)
-::                        Connection port will be port+2
+:: -port=7000        : Start server on specified port (default is 7779)
+::                  Connection port will be port+2
 ::
-::-MaxPlayers-64        ：Override max players set in ServerSettings, ini
+:: -MaxPlayers=64    : Override max players set in ServerSettings.ini
 ::
-:: -nobattleye          :Lounch server without Battleye (not reconmended)
+:: -nobattleye       : Launch server without Battleye (not recommended)
 pause
